46aaa77cbbd483fc7886e6713a8e6119cc626586
MatteoLatoile <me2llincontact@gmail.com>
2026-07-11 11:50:11 +0200

    remplace next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  devIndicators: {
-    position: "top-left", // ou 'top-right' / 'bottom-left' / 'bottom-right'
-  },
-  // Tu veux vraiment le désactiver totalement :
-  experimental: {
-    nextDev: {
-      devPanel: false, // 💣 ça vire le badge dev complet
-    },
-  },
-};
+/** @type {import("next").NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
